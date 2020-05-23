440bdf5e3fcb8fe9215ece83ef566bd5172aa235
Rafdapp <raf.dapp@gmail.com>
2020-05-23 21:53:24 +0100

    hooked button

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./App.css"
 import Todo from "./Todo"
 
 function App() {
+  const [input, setInput] = useState("");
+  const addTodo = (e) => {
+    console.log('BOOM >' ,input);
+  } 
+
   return (
     <div className="App">
       <h1>Todo app</h1>
-      <input type="text"/>
-      <button>Add todo</button>
+      <input value={input} onChange = {e => setInput(e.target.value) }type="text"/>
+      <button onClick = {addTodo}>Add todo</button>
       <Todo title="Take dogs"/>
     </div>  
   );
