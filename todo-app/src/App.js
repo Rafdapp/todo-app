715773a2e3a4a2a4d51b8b2050d9324beaf18924
Rafdapp <raf.dapp@gmail.com>
2020-05-23 22:06:43 +0100

    setTodos, spread opreator

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -3,16 +3,23 @@ import "./App.css"
 import Todo from "./Todo"
 
 function App() {
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState(""); //short memory
+  const [todos, setTodos] = useState([]); //short memory
   const addTodo = (e) => {
     console.log('BOOM >' ,input);
+    // 1 add whateever in input to the todos array
+    // 2 clear the input field
+
+    setTodos([...todos, input]);
+    setInput("");
+    console.log([...todos, input]);
   } 
 
   return (
     <div className="App">
       <h1>Todo app</h1>
       <input value={input} onChange = {e => setInput(e.target.value) }type="text"/>
-      <button onClick = {addTodo}>Add todo</button>
+      <button onClick = {addTodo}>Add todo</button>; 
       <Todo title="Take dogs"/>
     </div>  
   );
