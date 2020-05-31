57fcca7453d778c4709dadd10766ed20ba2f284e
Rafdapp <raf.dapp@gmail.com>
2020-05-31 21:32:33 +0100

    added scripts for  task delete button

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -3,32 +3,48 @@ import "./App.css"
 import Todo from "./Todo"
 
 function App() {
+
+  function deleteTodo(e) {
+    e.preventDefault();
+    let index = e.target.value;
+    todos.splice(index, 1);
+    setTodos([...todos]);
+  }
   const [input, setInput] = useState(""); //short memory
   const [todos, setTodos] = useState([]); //short memory
   const addTodo = (e) => {
-    console.log('BOOM >' ,input);
+   // console.log('BOOM >' ,input);
     // 1 add whateever in input to the todos array
     // 2 clear the input field
 
     setTodos([...todos, input]);
     setInput("");
-    console.log([...todos, input]);
+    e.preventDefault()  // prevent automatic refresh form
+    //console.log([...todos, input]);
   } 
 
   return (
     <div className="App">
       <h1>Todo app</h1>
-      <input 
-        value={input} 
-        onChange = {(e) => setInput(e.target.value) }
-        type="text"
-      />
-      <button onClick = {addTodo}>Add todo</button>; 
-      {todos.map(todo => {
-        return (
-          <Todo title ={todo} />
-        )
-      })}
+      <form>
+          <input 
+            value={input} 
+            onChange = {(e) => setInput(e.target.value) }
+            type="text"
+          />
+          <button disabled={!input} type="submit" onClick = {addTodo}>Add todo</button>; 
+        
+        
+          {todos.map((todo, i) => {
+            return (
+              <Todo title ={todo} key ={i}
+                                  value ={i}
+                                  deleteTodo ={deleteTodo}
+              />
+
+          )
+        })}
+      </form>
     </div>  
   );
 }
--- todo-app/src/Todo.js
+++ todo-app/src/Todo.js
@@ -5,6 +5,7 @@
      return (
          <div>
              <h1>{props.title}</h1>
+             <button className="deleteBtn" value={props.value} onClick={props.deleteTodo}>DELETE</button>
          </div>
      );
  };
